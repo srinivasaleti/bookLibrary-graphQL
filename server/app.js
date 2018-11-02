49e6afe9f763ebcdc6bf7c42b7c59027e47955b1
Srinivas <srinivas.aleti03@gmail.com>
2018-11-02 16:55:40 +0530

    Connect to mongoose

--- server/app.js
+++ server/app.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const schema = require('./schema/schema')
+const mongoose = require('mongoose')
 
 const app = express()
 
+//connection to mongodb
+mongoose.connect("mongodb://srinu:test123@ds149593.mlab.com:49593/srinu")
+mongoose.connection.once('open', () => {
+    console.log("Connected to database")
+})
+
+//graphql
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true
